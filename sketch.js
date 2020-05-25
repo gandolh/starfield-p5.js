00ea239eda6bdd068d997c8e149a669e01e0e19e
gandolh <gusatualex@yahoo.com>
2020-05-26 01:01:33 +0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+//inspired from The coding train/Daniel Shiffman
+//this man is a fucking genius
+let stars = [];
+
+let speed;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  for (let i = 0; i < 400; i++) {
+    stars[i] = new Star();
+  }
+}
+
+function draw() {
+  speed = map(mouseX, 0, width, 0, 50);
+  background(0);
+  translate(width / 2, height / 2);
+  for (let i = 0; i < stars.length; i++) {
+    stars[i].update();
+    stars[i].show();
+  }
+}
+function star(x, y, radius1, radius2, npoints) {
+  var angle = TWO_PI / npoints;
+  var halfAngle = angle / 2.0;
+  beginShape();
+  for (var a = 0; a < TWO_PI; a += angle) {
+    var sx = x + cos(a) * radius2;
+    var sy = y + sin(a) * radius2;
+    vertex(sx, sy);
+    sx = x + cos(a + halfAngle) * radius1;
+    sy = y + sin(a + halfAngle) * radius1;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+}
---
+++ star.js
@@ -0,0 +1,39 @@
+class Star {
+  constructor() {
+    this.x = random(-width, width);
+    this.y = random(-height, height);
+    this.z = random(width);
+    this.pz = this.z;
+    this.re=random(255);
+    this.ge=random(255);
+    this.be=random(255);
+  }
+  update() {
+        this.z = this.z - speed;
+    if (this.z < 1) {
+      this.z = width;
+      this.x = random(-width, width);
+      this.y = random(-height, height);
+      this.pz = this.z;
+    }
+  }
+  show() {
+ fill(this.re,this.ge,this.be);
+    noStroke();
+
+    var sx = map(this.x / this.z, 0, 1, 0, width);
+    var sy = map(this.y / this.z, 0, 1, 0, height);
+
+    var r = map(this.z, 0, width, 25, 0);
+    star(sx,sy,r,r/3,5);
+    //ellipse(sx, sy, r, r);
+
+    var px = map(this.x / this.pz, 0, 1, 0, width);
+    var py = map(this.y / this.pz, 0, 1, 0, height);
+
+    this.pz = this.z;
+
+    stroke(255);
+    line(px, py, sx, sy);
+  }
+}
